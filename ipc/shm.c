1815884e32188d318fd2c39c8893b736686d50cd
Mayra Guajardo <31485345+mayraguajardo@users.noreply.github.com>
2019-10-01 20:37:51 -0500

    Create shm.c

---
+++ ipc/shm.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <unistd.h>
+
+int main(void) {
+  int pid;
+  pid = fork();
+  if(pid == 0){
+    // Proceso escritor
+    int key = 17;
+    int shmid = shmget(key,100,IPC_CREAT|0666);
+    char * message = (char *)shmat(shmid,NULL,0);
+    sprintf(message, "Hola Mundo");
+    while(message[0] == 'H'){
+      sleep(1);
+    }
+    printf("El mensaje final es: %s\n",message);
+  } else {
+    int key = 17;
+    int shmid = shmget(key,100,IPC_CREAT|0666);
+    char * message = (char *)shmat(shmid,NULL,0);
+    sleep(1);
+    printf("El mensaje es: %s \n",message);
+    message[0] = '*';
+    sleep(1);
+  }
+}
